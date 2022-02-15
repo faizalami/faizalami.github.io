7317f1e9ef9460960eb41422f6b5dac1c34850de
Faizal Amiruddin <faizal@wowrack.com>
2022-02-15 09:53:35 +0700

    about section

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="about" class="flex bg-white">
-    <div class="grid md:grid-cols-3 gap-8 m-4 md:m-8">
-      <div class="flex flex-col p-4 mt-48 border-gray-600 border-2 items-center">
-        <div class="relative photo -mt-40">
+    <div class="grid md:grid-cols-3 md:grid-rows-2 gap-8 m-4 md:m-8">
+      <div class="md:row-span-2 flex flex-col p-4 mt-28 border-gray-600 border-2 items-center">
+        <div class="relative photo -mt-32">
           <picture>
             <source type="image/webp" srcset="~/assets/img/photo.webp">
             <source srcset="~/assets/img/photo.png">
@@ -27,12 +27,61 @@
         </div>
       </div>
 
-      <div class="p-4 border-gray-600 border-2">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nesciunt repellat tenetur totam velit. Commodi consequatur consequuntur dolor incidunt odit perspiciatis totam! Accusantium cupiditate enim est inventore nemo recusandae, voluptatem.
+      <div class="flex flex-col mt-6 p-4 border-gray-600 border-2">
+        <h2 class="text-xl font-bold mb-4 mx-auto -mt-10 p-2 border-gray-600 border-2 bg-white">
+          <img class="inline-block skill-icon" src="~/assets/img/icon/star-symbol.png" alt="">
+          Advanced Skills
+        </h2>
+        <ul class="list-disc ml-4">
+          <li>Javascript</li>
+          <li>Vue</li>
+          <li>React</li>
+        </ul>
       </div>
 
-      <div class="p-4 border-gray-600 border-2">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nesciunt repellat tenetur totam velit. Commodi consequatur consequuntur dolor incidunt odit perspiciatis totam! Accusantium cupiditate enim est inventore nemo recusandae, voluptatem.
+      <div class="flex flex-col md:row-start-2 md:col-start-2 mt-6 p-4 border-gray-600 border-2">
+        <h2 class="text-xl font-bold mb-4 mx-auto -mt-10 p-2 border-gray-600 border-2 bg-white">
+          <img class="inline-block skill-icon" src="~/assets/img/icon/star-symbol-silver.png" alt="">
+          Other Skills
+        </h2>
+        <ul class="list-disc ml-4">
+          <li>PHP</li>
+          <li>Svelte</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>Graphic Design</li>
+          <li>Photo Editing</li>
+        </ul>
+      </div>
+
+      <div class="flex md:row-span-2 p-4 border-gray-600 border-2">
+        <div class="flex flex-col w-full h-full p-4 justify-center items-center border-gray-600 border-2">
+          <h2 class="text-xl font-bold mb-4 mx-auto p-2 border-gray-600 border-2 bg-white">
+            Contacts
+          </h2>
+          <ul class="flex flex-col space-y-4 mt-4 break-all text-sm md:text-base">
+            <li>
+              <img class="inline-block mr-2 object-contain contact-icon" src="~/assets/img/icon/pin-location.png" alt="Home Address">
+              Dlanggu, Kab. Mojokerto, Jawa Timur
+            </li>
+            <li>
+              <img class="inline-block mr-2 object-contain contact-icon" src="~/assets/img/icon/whatsapp-color.png" alt="Whatsapp Number">
+              <a href="https://wa.me/85330438009">+62 853 3043 8009 (Whatsapp)</a>
+            </li>
+            <li>
+              <img class="inline-block mr-2 object-contain contact-icon" src="~/assets/img/icon/gmail.png" alt="Email">
+              <a href="mailto:f.a.faizal.amiruddin@gmail.com">f.a.faizal.amiruddin@gmail.com</a>
+            </li>
+            <li>
+              <img class="inline-block mr-2 object-contain contact-icon" src="~/assets/img/icon/github.png" alt="Github Profile">
+              <a href="https://github.com/faizalami">github.com/faizalami</a>
+            </li>
+            <li>
+              <img class="inline-block mr-2 object-contain contact-icon" src="~/assets/img/icon/linkedin-square-color.png" alt="Linkedin Profile">
+              <a href="https://www.linkedin.com/in/faizalami/">linkedin.com/in/faizalami</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </section>
@@ -49,5 +98,16 @@ export default {
 #about .photo img {
   width: 250px;
   height: 250px;
+}
+
+.skill-icon {
+  height: 1.25rem;
+  margin-top: -0.5rem;
+}
+
+.contact-icon {
+  height: 1.25rem;
+  width: 1.25rem;
+  margin-top: -0.25rem;
 }
 </style>
